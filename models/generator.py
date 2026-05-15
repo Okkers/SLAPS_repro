d9429c4dbc9fb919901268853834f11a0583438b
Christina Zheng <C.T.T.Zheng@student.tudelft.nl>
2026-05-15 18:32:38 +0200

    Initial implementation of the three generator classes.

---
+++ models/generator.py
@@ -0,0 +1,85 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F 
+import utils.graph_utils as graph_utils
+
+
+class FullParametrization(nn.Module):
+    def __init__(self, features, k):
+        super(FullParametrization, self).__init__()
+
+        self.k = k
+
+        # Initialize the adjacency matrix A as a learnable parameter using the kNN graph
+        self.A = nn.Parameter(
+            torch.tensor(graph_utils.initialize_kNN_graph(features, k), dtype=torch.float32)
+        )
+
+    def forward(self, ):
+        return self.A
+    
+class MLP(nn.Module):
+    def __init__(self, layers_size, input_dim, k):
+        super(MLP, self).__init__()
+
+        # Note that the paper mentions that "we keep the input dimension the same throughout the layers."
+        self.input_dim = input_dim
+        self.output_dim = input_dim # the paper mentions that "for both variants, we initialize the weight matrices with identity"
+                                        # so the output dimension is probably the same as the input dimension.
+        self.k = k
+        
+        self.mlp = []
+
+        if layers_size == 1:
+            self.mlp.append(nn.Linear(input_dim, input_dim))
+        else:
+            for _ in range(layers_size - 1):
+                self.mlp.append(nn.Linear(input_dim, input_dim))
+                self.mlp.append(nn.ReLU())
+            self.mlp.append(nn.Linear(input_dim, input_dim))
+
+        self.mlp = nn.Sequential(*self.mlp)
+
+        self.initialize_MLP()
+
+    def forward(self, x):
+        # Pass the input through the MLP layers
+        x = self.mlp(x) 
+        
+        # We need kNN(MLP(X)), so we need to do the kNN operation
+        A_tilde = graph_utils.kNN_generator(x, self.k)
+
+        return A_tilde
+    
+    def initialize_MLP(self):
+        for layer in self.mlp:
+            if isinstance(layer, nn.Linear):
+                with torch.no_grad():
+                    layer.weight.copy_(torch.eye(self.input_dim))
+                    layer.bias.zero_()
+
+class MLP_D(nn.Module):
+    def __init__(self, layers_size, input_dim, k):
+        super(MLP_D, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = input_dim
+        self.k = k
+
+        self.mlp = nn.ParameterList()
+
+        for _ in range(layers_size):
+            self.mlp.append(nn.Parameter(torch.ones(input_dim)))
+
+
+    def forward(self, x):
+        # Pass the input through the MLP layers
+        for idx, weight in enumerate(self.mlp):
+            x = x * weight # since we have a diagonal matrix, we can just do element multiplication
+
+            if idx != len(self.mlp) - 1:
+                x = F.ReLU(x)
+        
+        # We need kNN(MLP(X)), so we need to do the kNN operation
+        A_tilde = graph_utils.kNN_generator(x, self.k)
+
+        return A_tilde
---
+++ utils/graph_utils.py
@@ -0,0 +1,26 @@
+from sklearn.neighbors import kneighbors_graph
+import numpy as np
+import torch.nn.functional as F
+import torch
+
+def initialize_kNN_graph(features, k):
+    A = kneighbors_graph(X=features, n_neighbors=k, mode='connectivity', metric='cosine', include_self=True)
+    A = A.toarray()
+    # A = A + np.eye(A.shape[0]) # Add self-loops
+    return A
+
+
+def kNN_generator(X,k):
+    # This is exactly M dot S described in the paper, but gradients will not flow:
+    # M_S = kneighbors_graph(X=X, n_neighbors=k+1, mode='distance', metric='cosine', include_self=True) 
+
+    X = F.normalize(X, p=2, dim=1) # Normalize the features to unit length
+    S = torch.mm(X, X.t()) # Compute the cosine similarity matrix; since we normalized, it is just the dot product
+    topk = torch.topk(S, k=k, dim=-1)
+    M = torch.zeros_like(S)
+    M.scatter_(dim=1, index=topk.indices, value=1.0)
+
+    M_S = M * S
+    return M_S
+    
+
