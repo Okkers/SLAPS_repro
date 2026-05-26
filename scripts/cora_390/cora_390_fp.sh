5901f0d2b3e6eb02186796e8e7d2cdbe120f23f3
zofiarynkiewicz <131818687+zofiarynkiewicz@users.noreply.github.com>
2026-05-26 22:36:12 +0200

    Add scripts for cora390

---
+++ scripts/cora_390/cora_390_fp.sh
@@ -0,0 +1,27 @@
+export CUDA_VISIBLE_DEVICES=1
+
+python -u main.py \
+    --is_training 1 \
+    --train_epochs 2000 \
+    --model_id CORA_TEST \
+    --dataset cora390 \
+    --model SLAPS_FP \
+    --input_dim 1433 \
+    --hidden_dim 32 \
+    --output_dim 7 \
+    --r 10 \
+    --eta 5 \
+    --noise_type "not_zero" \
+    --generator FP \
+    --gen_input_dim 1433 \
+    --gen_layers_size 2 \
+    --gen_k 20 \
+    --use_gpu True \
+    --lr_c 0.01 \
+    --lr_DAE 0.01 \
+    --dropout_c 0.25 \
+    --dropout_DAE 0.5 \
+    --lambda_val 100 \
+    --itr 10 \
+    --patience 500 \
+    --is_discrete
