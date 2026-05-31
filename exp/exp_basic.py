7824aa8597540b36ccfb810e0568246490bfa724
zofiarynkiewicz <131818687+zofiarynkiewicz@users.noreply.github.com>
2026-05-31 17:39:50 +0200

    fix device

--- exp/exp_basic.py
+++ exp/exp_basic.py
@@ -4,7 +4,7 @@ import torch
 class Exp_Basic(object):
     def __init__(self, args):
         self.args = args
-        self.device = torch.device("mps")
+        self.device = self._acquire_device()
         self.model = self._build_model().to(self.device)
 
     def _build_model(self):
